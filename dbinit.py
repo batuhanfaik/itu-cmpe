212caef2172d17d3415823de43aaadd4283d1562
H. Turgut Uyar <uyar@itu.edu.tr>
2018-08-27 15:15:20 +0300

    Add database initialization operations

---
+++ dbinit.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+import psycopg2 as dbapi2
+
+
+INIT_STATEMENTS = [
+    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
+    "INSERT INTO DUMMY VALUES (42)",
+]
+
+
+def initialize(url):
+    with dbapi2.connect(url) as connection:
+        cursor = connection.cursor()
+        for statement in INIT_STATEMENTS:
+            cursor.execute(statement)
+        cursor.close()
+
+
+if __name__ == "__main__":
+    url = os.getenv("DATABASE_URL")
+    if url is None:
+        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
+        sys.exit(1)
+    initialize(url)
