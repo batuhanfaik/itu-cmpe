e7de6a7fcb5b1591ac61161b76adfea49aff0926
Batuhan Faik Derinbay <batuhanfaik@gmail.com>
2018-12-15 16:14:35 +0300

    Web page implemented

--- bottle_app.py
+++ bottle_app.py
@@ -5,29 +5,65 @@
 ### The landing page for assignment 3 should be at /
 #####################################################################
 
-from bottle import route, run, default_app, debug
+from bottle import route, run, default_app, debug, static_file
+import indexHTML
 
-def htmlify(title,text):
-    page = """
-        <!doctype html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <title>%s</title>
-            </head>
-            <body>
-            %s
-            </body>
-        </html>
+# def htmlify(title,text):
+#     page = """
+#         <!doctype html>
+#         <html lang="en">
+#             <head>
+#                 <meta charset="utf-8" />
+#                 <title>%s</title>
+#             </head>
+#             <body>
+#             %s
+#             </body>
+#         </html>
+#
+#     """ % (title,text)
+#     return page
+#
+# def index():
+#     return htmlify("My lovely website",
+#                    "This is going to be an awesome website, when it is finished.")
 
-    """ % (title,text)
-    return page
-
+@route('/') #same thing with --> route('/', 'GET', index)
 def index():
-    return htmlify("My lovely website",
-                   "This is going to be an awesome website, when it is finished.")
+    return indexHTML.htmlifyIndex()
 
-route('/', 'GET', index)
+#HTML Routing
+@route('/basics_of_sailing/<htmlPath>')
+def html(htmlPath):
+    return static_file(htmlPath, root="./basics_of_sailing")
+
+#CSS Routing
+@route('/css/<cssPath>')
+def css(cssPath):
+    return static_file(cssPath, root="./css")
+
+#Media and Asset Routing
+@route('/img/<imgPath>')
+def img(imgPath):
+    return static_file(imgPath, root="./img")
+@route('/img/backgrounds/<bg>')
+def imgBg(bg):
+    return static_file(bg, root="./img/backgrounds")
+@route('/img/knots/<knot>')
+def imgKnot(knot):
+    return static_file(knot, root="./img/knots")
+@route('/img/rules/<rule>')
+def imgRule(rule):
+    return static_file(rule, root="./img/rules")
+@route('/img/stars/<star>')
+def imgStar(star):
+    return static_file(star, root="./img/stars")
+
+#Icon Routing (Doesn't work doe)
+@route('/img/sailboat_logo_32x32.ico')
+def logoIcon():
+    return static_file('sailboat_logo_32x32.ico', root='./img')
+
 
 #####################################################################
 ### Don't alter the below code.
@@ -41,5 +77,5 @@ debug(True)
 app = default_app()
 # The below code is necessary for running this bottle app standalone on your computer.
 if __name__ == "__main__":
-  run()
+  run(reloader=True)
 
