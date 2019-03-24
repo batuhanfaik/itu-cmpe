c1028eb04a1493028b455b1cccc4923c51e35094
Batuhan Faik Derinbay <batuhanfaik@gmail.com>
2019-03-24 19:28:30 +0300

    Methods moved to header files

--- Vector.h
+++ Vector.h
@@ -13,32 +13,115 @@ using namespace std;
 class Vector {
     int size;
     int* value_array;
-//    int vector_no;
-//    static int vectors_created;
 public:
     // Default constructor
-    Vector();
+    Vector():size(0),value_array(nullptr){}
+
     // Constructor
-    Vector(int,const int*);
-    // Getter methods
-    int getSize() const;
-    int getValueArray(int) const;
-//    int getVectorNo() const;
-    // Operator overload +
-    Vector operator+(const Vector&) const;
-    // Operator overload * (Dot product)
-    int operator*(const Vector&) const;
-    // Operator overload * (Scalar multiplication)
-    Vector operator*(int) const;
-    // Operator overload <<
-    friend ostream& operator<<(ostream&, const Vector&);
+    Vector(int size,const int * value_arr):size(size) {
+        // Allocate memory
+        value_array = new int[size];
+
+        // Copy values of the array
+        for (int i = 0; i < size; ++i) {
+            value_array[i] = value_arr[i];
+        }
+    }
+
     // Copy constructor
-    Vector(const Vector&);
-    // Temporary print
-    const void print() const;
-//     Clear vector counter
-//    static void clear_counter(){vectors_created = 0;};
-    ~Vector();
+    Vector(const Vector& vector_in) {
+        size = vector_in.size;
+        // Allocate memory for the new array
+        value_array = new int[size];
+        // Copy the values over
+        for (int i = 0; i < size; ++i) {
+            value_array[i] = vector_in.value_array[i];
+        }
+    }
+
+    // Getter methods
+    int getSize() const{
+        return size;
+    }
+    int getValueArray(int i) const{
+        return value_array[i];
+    }
+
+    // Operator overload +
+    Vector operator+(const Vector& vector_in) const{
+        // If sizes match
+        if (getSize() == vector_in.getSize()){
+            int* new_value_array;
+            new_value_array = new int[getSize()];
+            // Do the addition
+            for (int i = 0; i < getSize(); ++i) {
+                new_value_array[i] = getValueArray(i) + vector_in.getValueArray(i);
+            }
+            // Return the resulting vector
+            return Vector(getSize(), new_value_array);
+        // If sizes don't match
+        } else {
+            cout << "Vector sizes don't match" << endl;
+            // Return an empty vector
+            return Vector(0,nullptr);
+        }
+    }
+
+    // Operator overload * (Dot product)
+    int operator*(const Vector& vector_in) const{
+        // If sizes match
+        if (getSize() == vector_in.getSize()){
+            int* new_value_array;
+            new_value_array = new int[getSize()];
+            // Do the addition
+            int sum = 0;
+            for (int i = 0; i < getSize(); ++i) {
+                new_value_array[i] = getValueArray(i) * vector_in.getValueArray(i);
+                sum += new_value_array[i];
+            }
+            // Return the resulting vector
+            return sum;
+        // If sizes don't match
+        } else {
+            cout << "Vector sizes don't match" << endl;
+            // Return 0
+            return 0;
+        }
+    }
+
+    // Operator overload * (Scalar multiplication)
+    Vector operator*(const int scalar) const{
+        // Create a new value array
+        int* new_value_array;
+        new_value_array = new int[getSize()];
+        // Multiply values by the scalar
+        for (int i = 0; i < getSize(); ++i) {
+            new_value_array[i] = getValueArray(i)*scalar;
+        }
+        // Return the new vector
+        return Vector(getSize(), new_value_array);
+    }
+
+    // Operator overload <<
+    friend ostream& operator<<(ostream& stream, const Vector& vector_in){
+        vector_in.print();
+        return stream;
+    }
+
+    // Print function
+    const void print() const{
+        cout << "(";
+        for (int i = 0; i < (size - 1); ++i) {
+            cout << value_array[i] << ", ";
+        }
+        cout << value_array[size - 1] << ")" << endl;
+    }
+
+    // Destructor
+    ~Vector(){
+    //    --vectors_created;
+    //    delete[] value_array;
+    }
 };
 
 
