574f475c10272b61a8aa5be2939d2d8e2cb4541a
Batuhan Faik Derinbay <batuhanfaik@gmail.com>
2019-04-16 23:05:52 +0300

    Test cases improved

--- main.cpp
+++ main.cpp
@@ -16,8 +16,15 @@ int main(){
     OperatorGrid my_grid(7,10);
     ArithmeticOperator ao1(3,3,2,'+');
     ArithmeticOperator ao2(5,5,1,'/');
+    ArithmeticOperator ao3(5,6,1,'/');
+    ArithmeticOperator ao4(5,7,0,'x');
     my_grid.place_operator(&ao1);
     my_grid.place_operator(&ao2);
-//    my_grid.move_operator(4,6,'D',1);
+    my_grid.place_operator(&ao3);
+    my_grid.place_operator(&ao4);
+    cout << "Before move" << endl;
+    my_grid.print_operators();
+    my_grid.move_operator(6,4,'R',2);
+    cout << "After move" << endl;
     my_grid.print_operators();
 }
