9e4e6f88047860229e6cef23f81e07e490a89551
faati <fatihaltinpinar@gmail.com>
2019-09-30 22:52:04 +0300

    Add part2

--- Experiment 1/experiment1.asm
+++ Experiment 1/experiment1.asm
@@ -2,7 +2,7 @@
 
 SetupP1		bis.b 	#00h, &P1DIR ;P1.0 output
 Mainloop	xor.b	#001h, &P1OUT ; Toggle P1.0
-Wait		mov.w 	#050000, R15 ; Delay to R15
+Wait		mov.w 	#250000, R15 ; Delay to R15
 L1			dec.w	#R15 ; Decrement R15
 			jnz L1; Delay over?
 			jmp Mainloop; Again
@@ -10,5 +10,17 @@ L1			dec.w	#R15 ; Decrement R15
 
 
 ; PART 2
-SetupP2		bis.b 	#FFh, #P1DIR ;Enable lights
-Mainloop
+SetupP2		mov.b 	#FFh, &P1DIR ;Enable lights
+			mov.b	#FFh, &P2DIR ;Enable second column of lights
+Reset		mov.b	#01h, &P1OUT ;1st step
+			mov.b	#80h, &P2OUT ;1st step second column
+Mainloop	rla.b	P1OUT ;
+			add.b 	#01h, &P1OUT ;add 1
+			rra.b	P2OUT ; rotate arithmetic right 
+Wait		mov.w 	#250000, R15 ; Delay to R15
+L1			dec.w	#R15 ; Decrement R15
+			jnz L1; Delay over?
+			cmp.b	#FFh, P2OUT; check if p2out is FF (all lights are on)
+			jnz Reset ;Jump to reset
+			jmp Mainloop; Again			
+			
