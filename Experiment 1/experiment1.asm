cd674d79a33499a95607b94a9e8c73180cce4b87
faati <fatihaltinpinar@gmail.com>
2019-09-30 18:41:09 +0300

    Initial commit

---
+++ Experiment 1/experiment1.asm
@@ -0,0 +1,8 @@
+; PART 1
+
+SetupP1		bis.b 	#00h, &P1DIR ;P1.0 output
+Mainloop	xor.b	#001h, &P1OUT ; Toggle P1.0
+Wait		mov.w 	#050000, R15 ; Delay to R15
+L1			dec.w	#R15 ; Decrement R15
+			jnz L1; Delay over?
+			jmp Mainloop; Again
