327e3e05cd143df1426abe62ad286b86035712a8
Fatih Altinpinar <fatihaltinpinar@gmail.com>
2019-11-10 12:56:54 +0300

    Reorder queries

--- dbinit.py
+++ dbinit.py
@@ -37,98 +37,6 @@ INIT_STATEMENTS = [
         ((value >= 15) and (value <=28))
     );
     
-    CREATE TABLE IF NOT EXISTS STUDENT (
-        tr_id BIGINT PRIMARY KEY references PEOPLE(tr_id) NOT NULL,
-        faculty_id int references FACULTY(id) not null,
-        department_id int references DEPARTMENT(id) not null,
-        student_id bigint not null,
-        semester smallint not null default 1,
-        grade smallint not null default 1,
-        gpa real not null default 0,
-        credits_taken credit not null,
-        minor boolean not null default false,
-        unique (student_id)
-    );
-    
-    CREATE TABLE IF NOT EXISTS ASSISTANT (
-        tr_id BIGINT PRIMARY KEY references PEOPLE(tr_id) NOT NULL,
-        faculty_id int references FACULTY(id) not null,
-        supervisor bigint references INSTRUCTOR(tr_id) not null,
-        assistant_id bigint not null,
-        bachelors varchar(80) not null,
-        degree varchar(80) not null,
-        grad_gpa real not null,
-        research_area varchar(100) not null,
-        office_day varchar(9) null default 'None',
-        office_hour_start time null,
-        office_hour_end time null,
-        unique (assistant_id)
-    );
-    
-    CREATE TABLE IF NOT EXISTS COURSES_ASSISTED (
-        crn char(6) primary key references COURSE(crn) not null,
-        assistant_id bigint references ASSISTANT(assistant_id) not null,
-        room_id int references CLASSROOM(classroom_id) not null,
-        problem_session boolean not null default false,
-        exam boolean not null default false,
-        homework boolean not null default false,
-        quiz boolean not null default false,
-        recitation boolean not null default false,
-        role varchar(60) null
-    );
-    
-
-    CREATE TABLE IF NOT EXISTS COURSE (
-        crn CHAR(6) NOT NULL PRIMARY KEY,
-        start_time TIME NOT NULL,
-        end_time TIME NOT NULL,
-        day VARCHAR(9) NOT NULL,
-        capacity INT NOT NULL,
-        enrolled INT NOT NULL,
-        credits REAL NOT NULL,
-        language CHAR(2) NOT NULL,
-        classroom_id INT NOT NULL,
-        faculty_id INT NOT NULL,
-        instructor_id BIGINT NOT NULL,
-        FOREIGN KEY (classroom_id) REFERENCES CLASSROOM (classroom_id),
-        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id),
-        FOREIGN KEY (instructor_id) REFERENCES INSTRUCTOR (tr_id)
-    );
-
-    CREATE TABLE IF NOT EXISTS INSTRUCTOR(
-        tr_id BIGINT NOT NULL PRIMARY KEY,
-        department_id INT NOT NULL,
-        faculty_id INT NOT NULL,
-        specialization VARCHAR(80),
-        bachelors VARCHAR(80),
-        masters VARCHAR(80),
-        doctorates VARCHAR(80),
-        room_id CHAR(4),
-        FOREIGN KEY (tr_id) REFERENCES PEOPLE (tr_id),
-        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id),
-        FOREIGN KEY (department_id) REFERENCES DEPARTMENT (id)
-    );
-
-    CREATE TABLE IF NOT EXISTS CLASSROOM(
-        classroom_id INT NOT NULL PRIMARY KEY,
-        capacity INT NOT NULL,
-        has_projection BOOLEAN NOT NULL,
-        door_number CHAR(4) NOT NULL,
-        floor VARCHAR(2) NOT NULL,
-        renewed BOOLEAN DEFAULT false,
-        board_count CHAR(1),
-        air_conditioner BOOLEAN DEFAULT false,
-        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id)
-    );
-
-    CREATE TABLE IF NOT EXISTS TAKEN_COURSES(
-        id INT PRIMARY KEY,
-        student_id BIGINT NOT NULL,
-        crn CHAR(6) NOT NULL,
-        datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (student_id) REFERENCES STUDENT (tr_id),
-        FOREIGN KEY (crn) REFERENCES COURSE (crn) 
-    );
 
     CREATE TABLE IF NOT EXISTS CAMPUS(
         id		        SERIAL 		NOT NULL,
@@ -138,7 +46,7 @@ INIT_STATEMENTS = [
         size 		    INT,
         PRIMARY KEY(id)
     );
-
+    
     CREATE TABLE IF NOT EXISTS FACULTY(
         id				    SERIAL 		NOT NULL,
         name 				VARCHAR(40)	NOT NULL,
@@ -160,6 +68,100 @@ INIT_STATEMENTS = [
         phone_number		CHAR(11),
         PRIMARY KEY(id),
         FOREIGN KEY(faculty_id) REFERENCES FACULTY(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS CLASSROOM(
+        classroom_id INT NOT NULL PRIMARY KEY,
+        capacity INT NOT NULL,
+        has_projection BOOLEAN NOT NULL,
+        door_number CHAR(4) NOT NULL,
+        floor VARCHAR(2) NOT NULL,
+        renewed BOOLEAN DEFAULT false,
+        board_count CHAR(1),
+        air_conditioner BOOLEAN DEFAULT false,
+        faculty_id INT,
+        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id)
+    );
+
+
+    CREATE TABLE IF NOT EXISTS STUDENT (
+        tr_id BIGINT PRIMARY KEY references PEOPLE(tr_id) NOT NULL,
+        faculty_id int references FACULTY(id) not null,
+        department_id int references DEPARTMENT(id) not null,
+        student_id bigint not null,
+        semester smallint not null default 1,
+        grade smallint not null default 1,
+        gpa real not null default 0,
+        credits_taken credit not null,
+        minor boolean not null default false,
+        unique (student_id)
+    );
+
+    CREATE TABLE IF NOT EXISTS INSTRUCTOR(
+        tr_id BIGINT NOT NULL PRIMARY KEY,
+        department_id INT NOT NULL,
+        faculty_id INT NOT NULL,
+        specialization VARCHAR(80),
+        bachelors VARCHAR(80),
+        masters VARCHAR(80),
+        doctorates VARCHAR(80),
+        room_id CHAR(4),
+        FOREIGN KEY (tr_id) REFERENCES PEOPLE (tr_id),
+        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id),
+        FOREIGN KEY (department_id) REFERENCES DEPARTMENT (id)
+    );
+    
+    CREATE TABLE IF NOT EXISTS ASSISTANT (
+        tr_id BIGINT PRIMARY KEY references PEOPLE(tr_id) NOT NULL,
+        faculty_id int references FACULTY(id) not null,
+        supervisor bigint references INSTRUCTOR(tr_id) not null,
+        assistant_id bigint not null,
+        bachelors varchar(80) not null,
+        degree varchar(80) not null,
+        grad_gpa real not null,
+        research_area varchar(100) not null,
+        office_day varchar(9) null default 'None',
+        office_hour_start time null,
+        office_hour_end time null,
+        unique (assistant_id)
+    );
+    
+    CREATE TABLE IF NOT EXISTS COURSE (
+        crn CHAR(6) NOT NULL PRIMARY KEY,
+        start_time TIME NOT NULL,
+        end_time TIME NOT NULL,
+        day VARCHAR(9) NOT NULL,
+        capacity INT NOT NULL,
+        enrolled INT NOT NULL,
+        credits REAL NOT NULL,
+        language CHAR(2) NOT NULL,
+        classroom_id INT NOT NULL,
+        faculty_id INT NOT NULL,
+        instructor_id BIGINT NOT NULL,
+        FOREIGN KEY (classroom_id) REFERENCES CLASSROOM (classroom_id),
+        FOREIGN KEY (faculty_id) REFERENCES FACULTY (id),
+        FOREIGN KEY (instructor_id) REFERENCES INSTRUCTOR (tr_id)
+    );
+    
+    CREATE TABLE IF NOT EXISTS COURSES_ASSISTED (
+        crn char(6) primary key references COURSE(crn) not null,
+        assistant_id bigint references ASSISTANT(assistant_id) not null,
+        room_id int references CLASSROOM(classroom_id) not null,
+        problem_session boolean not null default false,
+        exam boolean not null default false,
+        homework boolean not null default false,
+        quiz boolean not null default false,
+        recitation boolean not null default false,
+        role varchar(60) null
+    );
+
+    CREATE TABLE IF NOT EXISTS TAKEN_COURSES(
+        id INT PRIMARY KEY,
+        student_id BIGINT NOT NULL,
+        crn CHAR(6) NOT NULL,
+        datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (student_id) REFERENCES STUDENT (tr_id),
+        FOREIGN KEY (crn) REFERENCES COURSE (crn) 
     );
 
     CREATE TABLE IF NOT EXISTS ADMINISTRATOR(
