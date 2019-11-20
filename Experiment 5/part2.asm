55a43f4ad8a5e9449b0c6ae07b21e5422b7ad460
Batuhan Faik Derinbay <batuhanfaik@gmail.com>
2019-11-20 13:50:00 +0300

    Hotfix part2

--- Experiment 5/part2.asm
+++ Experiment 5/part2.asm
@@ -33,14 +33,14 @@ Setup		bis.b	#0FFh,		&P1DIR
 Main		cmp		#arr_end, 	r4
 			jz		Reset_seq
 			mov.b	@r4,		&P1OUT
-			call	Delay
+			call	#Delay
 			add		#001h,		r4
 			jmp		Main
 
 
 ; Reset sequence
-Reset_seq	mov.b	#arr,		r4
-			jmp       Main
+Reset_seq	mov.w	#arr,		r4
+			jmp     Main
 
 ; Delay function
 Delay		push 	r14
@@ -58,12 +58,14 @@ L1			dec.w	R15
 ;						0			1		   2	     3			4			5		  6			  7			8		   9
 arr			.byte	00111111b, 00000110b, 01011011b, 01001111b, 01100110b, 01101101b, 01111101b, 00000111b, 01111111b, 01101111b
 arr_end
+
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
             .global __STACK_END
             .sect   .stack
             
+
 ;-------------------------------------------------------------------------------
 ; Interrupt Vectors
 ;-------------------------------------------------------------------------------
