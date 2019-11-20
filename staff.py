d2019d5829728381ceed1fbe19f8e746661ce74f
Zeynep Yetiştiren <36111438+zynpyetistiren@users.noreply.github.com>
2019-11-20 19:37:36 +0300

    added staff table

---
+++ staff.py
@@ -0,0 +1,8 @@
+class Staff:
+    def __init__(self,tr_id,faculty_id,facility_id,duty,authority_lvl,department):
+        self.faculty_id = faculty_id
+        self.facility_id = facility_id
+        self.tr_id = tr_id
+        self.duty = duty
+        self.authority_lvl = authority_lvl
+        self.department = department
