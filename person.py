226eaf241bef59c3f1c7181ec76317a81dd9a7d5
Batuhan Faik Derinbay <batuhan@derinbay.com>
2019-11-20 22:41:36 +0300

    Login structure of person object defined

--- person.py
+++ person.py
@@ -6,6 +6,7 @@
 # @ Project: itucsdb1922
 # @ Description: Not available
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from flask import current_app
 from flask_login import UserMixin
 
 
@@ -13,7 +14,6 @@ class Person(UserMixin):
     def __init__(self, tr_id, name, surname, phone_number, email, password, person_category,
                  mother_fname, father_fname, gender, birth_city, birth_date, id_reg_city,
                  id_reg_district):
-
         self.tr_id = tr_id
         self.name = name
         self.surname = surname
@@ -28,3 +28,26 @@ class Person(UserMixin):
         self.birth_date = birth_date
         self.id_reg_city = id_reg_city
         self.id_reg_district = id_reg_district
+
+        self.active = True
+        if self.person_category == 0:
+            self.is_admin = True
+
+        def get_id(self):
+            return self.email
+
+        @property
+        def is_active(self):
+            return self.active
+
+
+def get_person(tr_id, name, surname, phone_number, email, password, person_category,
+               mother_fname, father_fname, gender, birth_city, birth_date, id_reg_city,
+               id_reg_district):
+    password = current_app.config["PASSWORDS"].get(tr_id)
+    person = Person(tr_id, name, surname, phone_number, email, password, person_category,
+                    mother_fname, father_fname, gender, birth_city, birth_date, id_reg_city,
+                    id_reg_district) if password else None
+    if person is not None:
+        person.is_admin = person.email in current_app.config["ADMIN_USERS"]
+    return person
