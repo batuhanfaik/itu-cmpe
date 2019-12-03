517ac2f584451c00f3644a8f0ee47ae7b91fad8d
yetistiren15 <yetistiren15@itu.edu.tr>
2019-12-04 01:59:18 +0300

    staff_facil table added

--- staff.py
+++ staff.py
@@ -1,8 +1,9 @@
 class Staff:
-    def __init__(self,tr_id,faculty_id,facility_id,duty,authority_lvl,department):
-        self.faculty_id = faculty_id
-        self.facility_id = facility_id
-        self.tr_id = tr_id
-        self.duty = duty
+    def __init__(self,id,manager_name,absences,hire_date,social_sec_no,authority_lvl,department):
+        self.manager_name = manager_name
+        self.id = id
+        self.absences = absences
+        self.hire_date = hire_date
         self.authority_lvl = authority_lvl
         self.department = department
+        self.social_sec_no = social_sec_no
