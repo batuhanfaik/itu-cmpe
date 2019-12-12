f0f66a903fd3c2372ed2881755e24e4f29ce0a0d
Cihat Akkiraz <by-waryoq@hotmail.com>
2019-12-12 11:51:49 +0300

    Url for faculty detailed page is added

--- server.py
+++ server.py
@@ -81,6 +81,8 @@ def create_app(db_url):
                      view_func=campus_views.campus_detailed, methods=['GET', 'POST'])
     app.add_url_rule("/faculty/<faculty_id>",
                      view_func=campus_views.faculty_detailed, methods=['GET', 'POST'])
+    app.add_url_rule("/department/<department_id>",
+                     view_func=campus_views.department_detailed, methods=['GET', 'POST'])
     app.add_url_rule("/test",
                      view_func=views.test_page, methods=['GET', 'POST'])
 
