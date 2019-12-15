1a7b850893f38c24b6b559e4f27de8587169d6ab
Cihat Akkiraz <by-waryoq@hotmail.com>
2019-12-15 20:25:52 +0300

    Taken_course is added

---
+++ taken_course.py
@@ -0,0 +1,6 @@
+class TakenCourse():
+    def __init__(self, id, student_id, crn, datetime):
+        self.id = id
+        self.student_id = student_id
+        self.crn = crn
+        self.grade = grade
