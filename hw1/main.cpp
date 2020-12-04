07712ccd294cf22f0d28720542551e34756e53ac
batuhanfaik <batuhanfaik@gmail.com>
2020-12-04 19:13:04 +0300

    Finish quicksort

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -4,10 +4,20 @@
 #include <vector>   // Unnecessary flex
 
 #include "sale.h"
+#include "quicksort.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    int N = 100;
+    if (argc < 2){
+        cout << "An N value needs to be passed in.\nThis run will assume that N=100" << endl;
+    } else if (argc > 2){
+        cout << "More than one parameters are passed in.\nThis run will assume that N=100" << endl;
+    } else {
+        N = stoi(argv[1]);
+        cout << "N=" << N << endl;
+    }
 
     ifstream file;
     file.open("sales.txt");
@@ -17,7 +27,6 @@ int main() {
         exit(1);
     }
 
-    int N = 100; //you should read value of N from command line
     string line, country, item_type, order_id;
 	int units_sold;
 	float total_profit;
@@ -38,6 +47,8 @@ int main() {
         Sale sale_item = Sale(country, item_type, order_id, units_sold, total_profit);
         sale_items.push_back(sale_item);
     }
+
+    quickSort(sale_items, 0, N-1);
 
     // Print sale items using a range based for loop
     for (const auto & sale_item : sale_items) {
--- hw1/quicksort.cpp
+++ hw1/quicksort.cpp
@@ -13,15 +13,22 @@
 
 using namespace std;
 
-int partition(vector<Sale>& sale_items, int start_index, int end_index){
+int partition(vector<Sale> &sale_items, int start_index, int end_index) {
     Sale pivot_element = sale_items.at(end_index);
     int pivot_index = start_index;
 
     // For each element in the partition check if the item needs to be on the left or right
     for (int i = start_index; i < end_index; i++) {
-        if (sale_items.at(i).get_units_sold() <= pivot_element.get_units_sold()) {
+        // Compare two strings, if sale[i] comes before pivot (comparison result -1) than they should be swapped
+        int str_comparison = sale_items.at(i).get_country().compare(pivot_element.get_country());
+        if (str_comparison < 0) {
             swap(sale_items[i], sale_items[pivot_index]);
             pivot_index++;
+        } else if (str_comparison == 0) {   // If the country is the same check the profit
+            if (sale_items.at(i).get_total_profit() > pivot_element.get_total_profit()) {
+                swap(sale_items[i], sale_items[pivot_index]);
+                pivot_index++;
+            }
         }
     }
     swap(sale_items.at(pivot_index), sale_items.at(end_index));
@@ -29,8 +36,8 @@ int partition(vector<Sale>& sale_items, int start_index, int end_index){
     return pivot_index;
 }
 
-void quickSort(vector<Sale>& sale_items, int start_index, int end_index){
-    if (start_index < end_index){
+void quickSort(vector<Sale> &sale_items, int start_index, int end_index) {
+    if (start_index < end_index) {
         int partition_index = partition(sale_items, start_index, end_index);
         quickSort(sale_items, start_index, partition_index - 1);
         quickSort(sale_items, partition_index + 1, end_index);
