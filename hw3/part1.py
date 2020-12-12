ded3986ea686ff8bcf875180babe692b0c17a2bd
batuhanfaik <batuhanfaik@gmail.com>
2020-12-12 14:24:03 +0300

    Add variable filter sizes and sum filtered images

--- hw3/part1.py
+++ hw3/part1.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     time.sleep(4)    # Wait until fullscreen notification is closed
     all_shapes_button = center_of_button_on_screen("assets/all-shapes-button.png", "all shapes")
     pyautogui.click(all_shapes_button)
+    time.sleep(0.1)    # Wait until shapes appear
 
     game_size = get_monitor_resolution()
     game_region = (0, 0, game_size[0], game_size[1])
@@ -48,14 +49,28 @@ if __name__ == "__main__":
     if not all_shapes_button[0] < game_size[0] and all_shapes_button[1] < game_size[1]:
         raise GameSizeNotCorrect("Please set the game_size variable manually.")
 
-    ss = pyautogui.screenshot(region=game_region)
-    ss_cv = np.array(ss)
-    ss_gray = cv2.cvtColor(ss_cv, cv2.COLOR_RGB2GRAY)
+    ss = np.array(pyautogui.screenshot(region=game_region))
+    ss_gray = cv2.cvtColor(ss, cv2.COLOR_RGB2GRAY)
 
-    sobel_x = cv2.Sobel(ss_gray, cv2.CV_64F, 1, 0, ksize=5)
-    sobel_y = cv2.Sobel(ss_gray, cv2.CV_64F, 0, 1, ksize=5)
-    cv2.imwrite("sobel_x.png", sobel_x)
-    cv2.imwrite("sobel_y.png", sobel_y)
+    # Define filters (sizes available are 3 and 5)
+    filter_size = 3
+    if filter_size == 5:
+        sobel_kernel_y = np.array([[1, 2, 3, 2, 1],
+                                   [2, 3, 5, 3, 2],
+                                   [0, 0, 0, 0, 0],
+                                   [-2, -3, -5, -3, -2],
+                                   [-1, -2, -3, -2, -1]])
+        sobel_kernel_x = np.transpose(sobel_kernel_y)
+    else:
+        sobel_kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+        sobel_kernel_x = np.transpose(sobel_kernel_y)
+
+    sobel_x = cv2.filter2D(ss_gray, -1, sobel_kernel_x)
+    sobel_y = cv2.filter2D(ss_gray, -1, sobel_kernel_y)
+    edge_magnitude = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
+    edge_magnitude *= 255 / np.max(edge_magnitude)
+
+    cv2.imwrite("output.png", edge_magnitude.astype(np.uint8))
 
     back_button = center_of_button_on_screen("assets/back-button.png", "back")
     pyautogui.click(back_button)
