edb042a4b95169f663077d377f76ca9f8ce33181
batuhanfaik <batuhanfaik@gmail.com>
2020-12-12 14:42:56 +0300

    Add methods.py

---
+++ hw3/common/methods.py
@@ -0,0 +1,56 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# @ Filename: methods
+# @ Date: 12-Dec-2020
+# @ AUTHOR: batuhanfaik
+# @ Copyright (C) 2020 Batuhan Faik Derinbay
+# @ Project: hw3
+# @ Description: Useful static methods
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import pyautogui
+import time
+
+from common.exceptions import GameNotInitiatedCorrectly, GameSizeNotCorrect
+
+
+def get_resolution():
+    w, h = pyautogui.size()
+    # If the width is more than twice the height there probably are multiple monitors
+    # For more than two monitors, the code is likely to fail
+    if w/h > 2/1:
+        w //= 2
+    return w, h
+
+
+def center_of_button_on_screen(button_img_path, button_name=""):
+    button = pyautogui.locateOnScreen(button_img_path)
+    if not button:
+        raise GameNotInitiatedCorrectly("Couldn't locate the {} button!".format(button_name))
+    else:
+        return pyautogui.center(button)
+
+
+def go_to_page(button_img_path, button_name=""):
+    fullscreen_button = center_of_button_on_screen("assets/fullscreen-button.png", "fullscreen")
+    pyautogui.click(fullscreen_button)
+    time.sleep(4)    # Wait until fullscreen notification is closed
+    button = center_of_button_on_screen(button_img_path, button_name)
+    pyautogui.click(button)
+    time.sleep(0.1)    # Wait until shapes appear
+    game_size = get_resolution()
+    game_region = (0, 0, game_size[0], game_size[1])
+
+    # Check if the button is within the game region
+    if not button[0] < game_size[0] and button[1] < game_size[1]:
+        raise GameSizeNotCorrect("Please set the game_size variable manually.")
+
+    return game_region
+
+
+def back_to_original_state():
+    try:
+        back_button = center_of_button_on_screen("assets/back-button.png", "back")
+        pyautogui.click(back_button)
+        pyautogui.press("esc")
+    except GameNotInitiatedCorrectly:
+        pyautogui.press("esc")
--- hw3/part1.py
+++ hw3/part1.py
@@ -11,44 +11,15 @@ import numpy as np
 import pyautogui
 import time
 
-from common.exceptions import GameNotInitiatedCorrectly, GameSizeNotCorrect
-
-
-def get_monitor_resolution():
-    w, h = pyautogui.size()
-    # If the width is more than twice the height there probably are multiple monitors
-    # For more than two monitors, the code is likely to fail
-    if w/h > 2/1:
-        w //= 2
-    return w, h
-
-
-def center_of_button_on_screen(button_img_path, button_name=""):
-    button = pyautogui.locateOnScreen(button_img_path)
-    if not button:
-        raise GameNotInitiatedCorrectly("Couldn't locate the {} button!".format(button_name))
-    else:
-        return pyautogui.center(button)
-
+import common.methods as game
 
 if __name__ == "__main__":
     # Switch to the game (Tested only on Firefox)
-    # time.sleep(5)
+    time.sleep(10)
 
-    fullscreen_button = center_of_button_on_screen("assets/fullscreen-button.png", "fullscreen")
-    pyautogui.click(fullscreen_button)
-    time.sleep(4)    # Wait until fullscreen notification is closed
-    all_shapes_button = center_of_button_on_screen("assets/all-shapes-button.png", "all shapes")
-    pyautogui.click(all_shapes_button)
-    time.sleep(0.1)    # Wait until shapes appear
-
-    game_size = get_monitor_resolution()
-    game_region = (0, 0, game_size[0], game_size[1])
-
-    # Check if the button is within the game region
-    if not all_shapes_button[0] < game_size[0] and all_shapes_button[1] < game_size[1]:
-        raise GameSizeNotCorrect("Please set the game_size variable manually.")
-
+    # Go to all shapes screen
+    all_shapes_button, button_name = "assets/all-shapes-button.png", "all shapes"
+    game_region = game.go_to_page(all_shapes_button, button_name)
     ss = np.array(pyautogui.screenshot(region=game_region))
     ss_gray = cv2.cvtColor(ss, cv2.COLOR_RGB2GRAY)
 
@@ -72,6 +43,4 @@ if __name__ == "__main__":
 
     cv2.imwrite("output.png", edge_magnitude.astype(np.uint8))
 
-    back_button = center_of_button_on_screen("assets/back-button.png", "back")
-    pyautogui.click(back_button)
-    pyautogui.press("esc")
+    game.back_to_original_state()
