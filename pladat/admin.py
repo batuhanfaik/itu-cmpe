aafee916080c7b6d92ff8791240bbcce3dbc013c
batuhanfaik <batuhanfaik@gmail.com>
2020-12-27 18:26:59 +0300

    Add student model with recsys satisfying inputs

---
+++ pladat/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from pladat.models import Student
+
+
+# Register your models here.
+admin.site.register(Student)
--- pladat_gr21/settings.py
+++ pladat_gr21/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pladat',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
