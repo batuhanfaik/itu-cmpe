f4383f17f3c748310fb49b4843f6e86b89553a46
baris <kaplany17@itu.edu.tr>
2021-01-01 13:54:47 +0000

    Add style to the forms

--- pladat/forms.py
+++ pladat/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
+USER_TYPE = (
+    (0, 'I want to find internships'),
+    (1, 'I want to find students for my job posting'),
+)
+
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(label='Please enter your email', required=True)
-    password = forms.CharField(widget=forms.PasswordInput(), label='Please enter your password', required=True)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), label='Please enter your email', required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Please enter your password', required=True)
+    user_type = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=USER_TYPE)
