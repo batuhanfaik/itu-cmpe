d3dd8962239981fab6432fe2e5f8d36331bf1eb0
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2021-01-14 23:47:06 +0300

    Add tests for pladat.RegistrationForm

--- apps/pladat/tests/test_forms.py
+++ apps/pladat/tests/test_forms.py
@@ -2,7 +2,95 @@ from django.test import SimpleTestCase
 from apps.pladat.forms import *
 
 
-class TestForms(SimpleTestCase):
+class TestRegistrationForm(SimpleTestCase):
+    def test_registration_form_valid_data(self):
+        form = LoginForm(data={
+            'email':'test@test.com',
+            'password':'password',
+            'first_name':'unit',
+            'last_name':'case',
+            'phone_number':'+905555555555',
+            'address':'my house is right on top of the world',
+            'city':'aksaray',
+            'state':'wat',
+            'country':'Turkey',
+            'user_type':'0'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_registration_form_no_data(self):
+        form = RegistrationForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 10)
+
+    def test_registration_no_email(self):
+        form = LoginForm(data={
+            'email': '',
+            'password': 'password',
+            'first_name': 'unit',
+            'last_name': 'case',
+            'phone_number': '+905555555555',
+            'address': 'my house is right on top of the world',
+            'city': 'aksaray',
+            'state': 'wat',
+            'country': 'Turkey',
+            'user_type': '0'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_registration_invalid_email_no_domain(self):
+        form = LoginForm(data={
+            'email': 'test',
+            'password': 'password',
+            'first_name': 'unit',
+            'last_name': 'case',
+            'phone_number': '+905555555555',
+            'address': 'my house is right on top of the world',
+            'city': 'aksaray',
+            'state': 'wat',
+            'country': 'Turkey',
+            'user_type': '0'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_registration_invalid_email_no_username(self):
+        form = LoginForm(data={
+            'email': '@test.com',
+            'password': 'password',
+            'first_name': 'unit',
+            'last_name': 'case',
+            'phone_number': '+905555555555',
+            'address': 'my house is right on top of the world',
+            'city': 'aksaray',
+            'state': 'wat',
+            'country': 'Turkey',
+            'user_type': '0'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+    def test_registration_no_password(self):
+        form = LoginForm(data={
+            'email': 'test@test.com',
+            'password': '',
+            'first_name': 'unit',
+            'last_name': 'case',
+            'phone_number': '+905555555555',
+            'address': 'my house is right on top of the world',
+            'city': 'aksaray',
+            'state': 'wat',
+            'country': 'Turkey',
+            'user_type': '0'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 1)
+
+
+
+class TestLoginForm(SimpleTestCase):
     def test_login_form_valid_data(self):
         form = LoginForm(data={
             'email': 'test@test.com',
