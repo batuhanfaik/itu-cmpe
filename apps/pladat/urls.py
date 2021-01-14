9265996ebd38fceaaa6f76029575a9b6ac3ee379
batuhanfaik <batuhanfaik@gmail.com>
2021-01-14 22:11:27 +0300

    Add 127.0.0.1 as an allowed host Add pladat urls unit tests

--- apps/pladat/urls.py
+++ apps/pladat/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import main_page_view, registration_view, login_page_view, recruiter_profile_view, recruiter_profile_update_view
 
 urlpatterns = [
-    path('', main_page_view),
-    path('register', registration_view),
-    path('login', login_page_view),
-    path('recruiter_profile', recruiter_profile_view),
-    path('recruiter_profile_update', recruiter_profile_update_view)
+    path('', main_page_view, name="main_page_view"),
+    path('register', registration_view, name="registration_view"),
+    path('login', login_page_view, name="login_page_view"),
+    path('recruiter_profile', recruiter_profile_view, name="recruiter_profile_view"),
+    path('recruiter_profile_update', recruiter_profile_update_view, name="recruiter_profile_update_view")
 ]
--- apps/pladat/views.py
+++ apps/pladat/views.py
@@ -10,6 +10,7 @@ from .forms import RegistrationForm, LoginForm
 from django.contrib.auth.models import User
 from .models import PladatUser
 
+
 def main_page_view(request):
     ctx = {}
     # if request.user.is_authenticated:
@@ -17,13 +18,16 @@ def main_page_view(request):
 
     return render(request, 'main_page.html', context=ctx)
 
+
 def recruiter_profile_view(request):
     ctx = {}
     return render(request, 'recruiter_profile.html', context=ctx)
 
+
 def recruiter_profile_update_view(request):
     ctx = {}
     return render(request, 'recruiter_profile_update.html', context=ctx)
+
 
 def login_page_view(request):
     if request.method == 'GET':
@@ -31,40 +35,40 @@ def login_page_view(request):
             return HttpResponse('Already logged in')
         login_form = LoginForm()
         ctx = {'form': login_form}
-        return render(request, 'user_login.html', context = ctx)
+        return render(request, 'user_login.html', context=ctx)
     elif request.method == 'POST':
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
             email = login_form.data['email']
             password = login_form.data['password']
             # We are using emails as also username
-            user = authenticate(username = email, password = password) 
+            user = authenticate(username=email, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('/')
             else:
                 login_form.add_error('Not user found with given email and password')
                 ctx['form'] = login_form
-                return render(request, 'user_login.html', context = ctx)
+                return render(request, 'user_login.html', context=ctx)
         else:
             ctx['form'] = login_form
-            return render(request, 'user_login.html', context = ctx)
+            return render(request, 'user_login.html', context=ctx)
     else:
         return HttpResponseForbidden('Forbidden method')
-
 
 
 def register_user(data):
     # Add a PladatUser
     user_dct = {
         'username': data['email'],
-        'email' : data['email'],
+        'email': data['email'],
         'password': data['password'],
     }
     user = User.objects.create_user(**user_dct)
     user.save()
 
-    fields = ['first_name', 'last_name', 'phone_number', 'address', 'city', 'state', 'country', 'user_type']
+    fields = ['first_name', 'last_name', 'phone_number', 'address', 'city', 'state', 'country',
+              'user_type']
     pladatuser_dct = {key: data[key] for key in fields}
     pladatuser_dct['user'] = user
 
@@ -72,15 +76,16 @@ def register_user(data):
     pladatuser.save()
 
     if data['user_type'] == PladatUser.UserType.STUDENT:
-        fields = ['degree', 'major', 'university', 'number_of_previous_work_experience', 'years_worked', 'is_currently_employed', 'skills_text']
+        fields = ['degree', 'major', 'university', 'number_of_previous_work_experience',
+                  'years_worked', 'is_currently_employed', 'skills_text']
         student_dct = {key: data[key] for key in fields}
         student_dct['pladatuser'] = pladatuser
 
         student = Student.objects.create(**student_dct)
         student.save()
-        
+
     elif data['user_type'] == PladatUser.UserType.COMPANY:
-        recruiter = Recruiter.objects.create(pladatuser = pladatuser)
+        recruiter = Recruiter.objects.create(pladatuser=pladatuser)
         recruiter.save()
 
     return True
@@ -88,7 +93,7 @@ def register_user(data):
 
 def registration_view(request):
     ctx = {}
-    
+
     if request.user.is_authenticated:
         # This happens when an logged in user visits the register page
         # We might consider redirecting here instead
@@ -98,26 +103,24 @@ def registration_view(request):
         # Unregistered user trying to access the registration form
         registration_form = RegistrationForm()
         ctx['form'] = registration_form
-        return render(request, 'user_register.html', context = ctx)
+        return render(request, 'user_register.html', context=ctx)
 
     elif request.method == 'POST':
         # Form filled and submitted
         registration_form = RegistrationForm(request.POST)
         if registration_form.is_valid():
             email = registration_form.data['email']
-            if User.objects.filter(email = email).exists():
+            if User.objects.filter(email=email).exists():
                 # Email already in use
                 registration_form.add_error('email', 'Email is already in use')
                 ctx['form'] = registration_form
-                return render(request, 'user_register.html', context = ctx)
+                return render(request, 'user_register.html', context=ctx)
             else:
                 register_user(registration_form.data)
                 return HttpResponse('Registered successfuly')
         else:
             # Something wrong with form
             ctx['form'] = registration_form
-            return render(request, 'user_register.html', context = ctx)
-    
+            return render(request, 'user_register.html', context=ctx)
+
     return HttpResponseForbidden()
-
-
