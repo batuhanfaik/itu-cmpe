f9bdf4aeaa71781d95705f895506a8f4903a1104
batuhanfaik <batuhanfaik@gmail.com>
2021-01-14 23:35:03 +0300

    Add pladat models unit tests

--- apps/pladat/tests/test_forms.py
+++ apps/pladat/tests/test_forms.py
@@ -1,45 +1,46 @@
 from django.test import SimpleTestCase
 from apps.pladat.forms import *
 
+
 class TestForms(SimpleTestCase):
     def test_login_form_valid_data(self):
         form = LoginForm(data={
-            'email':'test@test.com',
-            'password':'password'
+            'email': 'test@test.com',
+            'password': 'password'
         })
         self.assertTrue(form.is_valid())
 
     def test_login_form_no_data(self):
         form = LoginForm(data={})
         self.assertFalse(form.is_valid())
-        self.assertEqual(len(form.errors),2)
+        self.assertEqual(len(form.errors), 2)
 
     def test_login_form_no_email(self):
         form = LoginForm(data={
-            'password':'password'
+            'password': 'password'
         })
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors), 1)
 
     def test_login_form_no_password(self):
         form = LoginForm(data={
-            'email':'test@test.com'
+            'email': 'test@test.com'
         })
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors), 1)
 
     def test_login_form_invalid_email_no_domain(self):
         form = LoginForm(data={
-            'email':'test',
-            'password':'password'
+            'email': 'test',
+            'password': 'password'
         })
         self.assertFalse(form.is_valid())
-        self.assertEqual(len(form.errors),1)
+        self.assertEqual(len(form.errors), 1)
 
     def test_login_form_invalid_email_no_username(self):
         form = LoginForm(data={
-            'email':'@test.com',
-            'password':'password'
+            'email': '@test.com',
+            'password': 'password'
         })
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form.errors), 1)
