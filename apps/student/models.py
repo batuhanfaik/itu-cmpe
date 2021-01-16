d4ab80094d1aaecc89134b4f5965ffce84bc22dd
karaketir16 <karaketir16@itu.edu.tr>
2021-01-16 22:01:19 +0300

    make years positive, add form error fields

--- apps/student/models.py
+++ apps/student/models.py
@@ -31,6 +31,6 @@ class Student(models.Model):
     university = models.CharField(max_length=8, choices=UNIVERSITIES, null = True, help_text="University")
     
     # number_of_previous_work_experience = models.IntegerField(default=0, help_text="Number of Previous Work Experience")
-    years_worked = models.IntegerField(default=0, help_text="Years Worked")
+    years_worked = models.PositiveIntegerField(default=0, help_text="Years Worked")
     # is_currently_employed = models.BooleanField(default=False, help_text="Currently Employed")
 
