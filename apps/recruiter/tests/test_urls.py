89e62bd4980c204502386311ecfc36fdb44f9cc2
Fatih ALTINPINAR <fatihaltinpinar@gmail.com>
2021-01-21 23:09:08 +0300

    Create url tests for recruiter and student

--- apps/recruiter/tests/test_urls.py
+++ apps/recruiter/tests/test_urls.py
@@ -7,5 +7,5 @@ class TestUrls(SimpleTestCase):
     def setUp(self):
         pass
     def test_profile_update_view_url(self):
-        url = reverse("profile_update_view")
+        url = reverse("rec_profile_update_view")
         self.assertEqual(resolve(url).func, profile_update_view)
