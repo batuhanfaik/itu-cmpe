f5c59c4119f511816a8b968dbbe6d62f41b1856c
UgurKap <uguralikaplan@gmail.com>
2021-01-23 03:52:07 +0300

    Revert "Moved the global embeddings object into the class"
    This reverts commit 5e4c11d2978720d38cdc7641bdf9c6c92d492180.

--- apps/recommend/tests.py
+++ apps/recommend/tests.py
@@ -1,42 +1,31 @@
+from django.test import TestCase, SimpleTestCase
 import pickle
 import numpy as np
-
-from django.test import TestCase, SimpleTestCase
 from apps.recommend.rec_utils import *
 from apps.recommend.rec_model import *
-from apps.job.models import Job
-from apps.student.models import Student
+
+embeddings = None
+with open("apps/recommend/embeddings.p", "rb") as f:
+	embeddings = pickle.load(f)
 
 class TestRecUtils(SimpleTestCase):
-
-	def doc2vec_setup(self):
-		self.embeddings = None
-		with open("apps/recommend/embeddings.p", "rb") as f:
-			self.embeddings = pickle.load(f)
-
-	def create_features_setup(self):
-		pass
-
 	def test_doc2vec_shape(self):
 		inp_text = "Hello World"
-		doc2vec_output = doc2vec(inp_text, self.embeddings)
+		doc2vec_output = doc2vec(inp_text, embeddings)
 
 		assert doc2vec_output.shape == (1, 50)
 
 	def test_doc2vec_simple_input(self):
 		inp_text = "Hello World"
-		expected_output = (self.embeddings["hello"] + self.embeddings["world"])/2
-		doc2vec_output = doc2vec(inp_text, self.embeddings)
+		expected_output = (embeddings["hello"] + embeddings["world"])/2
+		doc2vec_output = doc2vec(inp_text, embeddings)
 
 		assert np.isclose(doc2vec_output, expected_output).all()
 
 	def test_doc2vec_word_order(self):
 		inp_text_1, inp_text_2 = "Hello World", "World Hello"
-		doc2vec_output_1 = doc2vec(inp_text_1, self.embeddings)
-		doc2vec_output_2 = doc2vec(inp_text_2, self.embeddings)
+		doc2vec_output_1 = doc2vec(inp_text_1, embeddings)
+		doc2vec_output_2 = doc2vec(inp_text_2, embeddings)
 
 		assert np.isclose(doc2vec_output_1, doc2vec_output_2).all()
-
-	def test_create_features_available_skills(self):
-		pass
 
