6148e2d31c9c6594dfbdb7692ee1581a82ed2008
batuhanfaik <batuhanfaik@gmail.com>
2021-01-23 18:42:59 +0300

    Add separate dataset and csv paths

--- loader.py
+++ loader.py
@@ -11,9 +11,9 @@ np.random.seed(1773)
 
 
 def load_input_img(filepath):
-	img = Image.open(filepath).convert('RGB')
-	#img = Image.open(filepath).convert('LA')
-	return img
+    img = Image.open(filepath).convert('RGB')
+    # img = Image.open(filepath).convert('LA')
+    return img
 
 
 class DataReader(torch.utils.data.Dataset):
@@ -37,31 +37,31 @@ class DataReader(torch.utils.data.Dataset):
 
         self.healthy = []
 
-		if mode == 'train':
-			self.input_transform = transforms.Compose([
-				#transforms.RandomHorizontalFlip(),
-				transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
-				transforms.RandomPerspective(distortion_scale = 0.1, interpolation=Image.BILINEAR),
-				transforms.ToTensor(),
-				transforms.Normalize(mean=(0.5, 0.5, 0.5),
-									 std=(0.5, 0.5, 0.5))
-			])
-		# TODO
-		elif mode == 'val':
-			self.input_transform = transforms.Compose([
-				transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
-				transforms.ToTensor(),
-				transforms.Normalize(mean=(0.5, 0.5, 0.5),
-									 std=(0.5, 0.5, 0.5))
-			])
-		# TODO
-		elif mode == 'test':
-			self.input_transform = transforms.Compose([
-				transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
-				transforms.ToTensor(),
-				transforms.Normalize(mean=(0.5, 0.5, 0.5),
-									 std=(0.5, 0.5, 0.5))
-			])
+        if mode == 'train':
+            self.input_transform = transforms.Compose([
+                # transforms.RandomHorizontalFlip(),
+                transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
+                transforms.RandomPerspective(distortion_scale=0.1, interpolation=Image.BILINEAR),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                     std=(0.5, 0.5, 0.5))
+            ])
+        # TODO
+        elif mode == 'val':
+            self.input_transform = transforms.Compose([
+                transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                     std=(0.5, 0.5, 0.5))
+            ])
+        # TODO
+        elif mode == 'test':
+            self.input_transform = transforms.Compose([
+                transforms.Resize(size=(224, 224), interpolation=Image.BILINEAR),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=(0.5, 0.5, 0.5),
+                                     std=(0.5, 0.5, 0.5))
+            ])
 
         if mode == 'train':
 
@@ -107,51 +107,51 @@ class DataReader(torch.utils.data.Dataset):
                 num_size = corona_counter - normal_counter
                 random_numbers = np.random.randint(len(self.healthy), size=num_size)
 
-					for i in random_numbers:
-						self.input_img_paths.append(self.healthy[i])
-						self.input_label.append(0)
-						normal_counter += 1
-					
-		elif mode == 'val':
+                for i in random_numbers:
+                    self.input_img_paths.append(self.healthy[i])
+                    self.input_label.append(0)
+                    normal_counter += 1
 
-			#val_input_folder = os.listdir("train")
-			val_input_folder = df['X_ray_image_name']
-			val_input_folder = sorted(val_input_folder)
+        elif mode == 'val':
+
+            # val_input_folder = os.listdir("train")
+            val_input_folder = df['X_ray_image_name']
+            val_input_folder = sorted(val_input_folder)
 
             normal_counter = 0
             corona_counter = 0
 
-			for i in range(len(val_input_folder)):
-				if self.dataset_type[val_input_folder[i]] == "VAL":
-					temp_path = "train/" + val_input_folder[i]#input image's path
-					self.input_img_paths.append(temp_path)
-					
-					if multi_class == True:
-						if self.label_1[val_input_folder[i]] == "Virus":
-							self.input_label.append(1)
-							corona_counter += 1
-						elif self.label_1[val_input_folder[i]] == "bacteria":
-							self.input_label.append(2)
-							corona_counter += 1
-						elif self.label_1[val_input_folder[i]] == "Stress-Smoking":
-							self.input_label.append(3)				
-							corona_counter += 1
-						else:
-							self.input_label.append(0)
-							self.healthy.append(temp_path)
-							normal_counter += 1
-					
-					else:
-						if self.label[val_input_folder[i]] == "Normal":
-							self.input_label.append(0)
-							self.healthy.append(temp_path)
-						else:
-							self.input_label.append(1)
-					
-		elif mode == 'test':
-			#test_input_folder = os.listdir("test")
-			test_input_folder = df['X_ray_image_name']
-			test_input_folder = sorted(test_input_folder)
+            for i in range(len(val_input_folder)):
+                if self.dataset_type[val_input_folder[i]] == "VAL":
+                    temp_path = "train/" + val_input_folder[i]  # input image's path
+                    self.input_img_paths.append(temp_path)
+
+                    if multi_class == True:
+                        if self.label_1[val_input_folder[i]] == "Virus":
+                            self.input_label.append(1)
+                            corona_counter += 1
+                        elif self.label_1[val_input_folder[i]] == "bacteria":
+                            self.input_label.append(2)
+                            corona_counter += 1
+                        elif self.label_1[val_input_folder[i]] == "Stress-Smoking":
+                            self.input_label.append(3)
+                            corona_counter += 1
+                        else:
+                            self.input_label.append(0)
+                            self.healthy.append(temp_path)
+                            normal_counter += 1
+
+                    else:
+                        if self.label[val_input_folder[i]] == "Normal":
+                            self.input_label.append(0)
+                            self.healthy.append(temp_path)
+                        else:
+                            self.input_label.append(1)
+
+        elif mode == 'test':
+            # test_input_folder = os.listdir("test")
+            test_input_folder = df['X_ray_image_name']
+            test_input_folder = sorted(test_input_folder)
 
             normal_counter = 0
             corona_counter = 0
