f1ea44770eac0e717c98f689d2c26e49ae9f90aa
batuhanfaik <batuhanfaik@gmail.com>
2021-01-26 13:57:52 +0300

    Add part1 implementation method

---
+++ part1/part1.py
@@ -0,0 +1,8 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# @ Filename: part1
+# @ Date: 26-Jan-2021
+# @ AUTHOR: batuhanfaik
+# @ Copyright (C) 2021 Batuhan Faik Derinbay
+# @ Project: VisionProject
+# @ Description: Play the Dice Game
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
