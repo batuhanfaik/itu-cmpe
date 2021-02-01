668dd57d336180daa6d72787ad12a4e6d24d1d0d
Atahan Özer <30796439+TrubadurOsman@users.noreply.github.com>
2021-02-01 21:25:19 +0300

    ABBVVVVV

--- part2/finito.py
+++ part2/finito.py
@@ -3,7 +3,7 @@ import dlib
 import cv2
 import pyautogui
 import time
-from datetime import datetime
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 def face_detect():
@@ -19,7 +19,7 @@ def face_detect():
         x2, y2 = pts.part(19).x, pts.part(19).y
         dist = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 1 / 2
         tmp = 1 if dist > 500 else 2
-        print(tmp)
+
         return tmp
     except:
 
@@ -41,7 +41,7 @@ def empty_check(direction):
     val = im[y,x]
     tmp = False if (val > 230 )^( val <80) else True
     stat = 'bos' if tmp else 'dolu'
-    print(stat)
+    # print(stat)
     return tmp
 
 def revert_direction(direction):
@@ -104,52 +104,78 @@ def next_point(direction,ind):
         return (ind[0],ind[1]+1)
     else:
         return (ind[0],ind[1]-1)
+def deadlock(grid,ind):
+    if grid[ind[0]-1,ind[1]] ==2 :
+        return 'A'
+    elif grid[ind[0]+1,ind[1]] ==2:
+        return 'D'
+    elif  grid[ind[0],ind[1]+1] ==2:
+        return 'W'
+    elif grid[ind[0],ind[1]-1]:
+        return 'S'
 
 count = 0
 dir = 'W'
 time.sleep(2)
 prev_bc = 1
 
-grid = np.zeros((100,100))# 1 for seen 2 for blocked
+grid = np.zeros((100,100))# 1 for blocked 2 seen
 ind = (50,50)#x y
-grid[50,50]= 1
-stack = []
+grid[50,50]= 2
 bug_counter = 0
+flag = 0
+
 while True:
+    # print(grid[50-5 : 50+5 ,50-5 : 50+5  ].T)
     if face_detect() ==0:
         exit()
     if grid[next_point(dir,ind)] == 0 and empty_check(dir) == False and face_detect() == 1 :
         bc = square_check(dir)
         # print(bc)
         if prev_bc != bc:
+            print('Rite of a Passage')
             prev_bc = bc
-            stack.append(ind)
-            grid[ind] = 2
             ind = next_point(dir,ind)
-            count = -2
+            grid[ind] = 2
+            count = 0
+            flag = 1
         move(dir,1,0.2)
         count+=1
         time.sleep(0.4)
         bug_counter = 0
+
+    elif grid[ind[0]+1,ind[1]] !=0  and grid[ind[0]-1,ind[1]] !=0  and grid[ind[0],ind[1]+1] !=0  and grid[ind[0],ind[1]-1] !=0 :
+        grid[ind] = 1
+        r = deadlock(grid,ind)
+        bc = square_check(dir)
+        if prev_bc != bc:
+            prev_bc = bc
+            ind = next_point(r,ind)
+        move(r, 1, 0.2)
+        time.sleep(0.1)
+        print('Life will find a way')
+
     elif grid[next_point(dir,ind)] !=0:
         dir = change_dir(dir)
+        print('que sera sera ')
 
-    elif (empty_check(dir) == True or face_detect() == 2) and bug_counter==0 :
-        print('geri bas')
+    elif (empty_check(dir) == True or face_detect() == 2) and bug_counter == 0:
+        print('YOU SHALL NOT PASS')
+        if flag == 1 :
+            count = (count//2)
+            flag =0
         grid[next_point(dir, ind)] = 1
         r = revert_direction(dir)
         for i in range(count):
             move(r, 1, 0.2)
             time.sleep(0.1)
         dir = change_dir(dir)
-        if next_point(dir,ind) == 2:
-            dir = change_dir(dir)
         count = 0
         bug_counter+=1
-    elif bug_counter != 0:
-            print('patladi')
+
+    elif bug_counter != 0 :
+            print('Ay Caramba')
             pyautogui.press(dir)
             pyautogui.press(revert_direction(dir))
 
 
-#TODO 1. MERKEZI KALIBRE ET 2. CIKMAZ SOKAK EKLE
