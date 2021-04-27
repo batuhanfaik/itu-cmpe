2c1fd30aafd765caba90e8079d2a3844585f74cd
batuhanfaik <batuhanfaik@gmail.com>
2021-04-27 20:30:13 +0300

    hope i get better at this soon

--- hw1/main.hs
+++ hw1/main.hs
@@ -1,14 +1,39 @@
 module Main where
-import System.Environment ( getArgs )
+import System.Environment(getArgs)
 import Data.Char(intToDigit, digitToInt)
 
-checkArgs [a, b, c] = parseArgs [a, b, c]
-checkArgs _ = putStrLn "At least three input arguments required"
+-- checkArgs a b (c:cs) 
+checkArgs :: [[Char]] -> [Char]
+checkArgs [] = []
+checkArgs _ = error "At least three input arguments required"
 
-parseArgs [a, b, c]
-    | a == "d2c" = putStrLn "Selam"
-    | otherwise = putStrLn "Other"
+parseArgs :: [Char] -> Int -> Int -> Char
+parseArgs mode base vals
+    | mode == "d2c" = digitsToChars base vals
+    | otherwise = error "Other"
+
+digitsToChars :: Int -> Int -> Char
+digitsToChars base dVal
+    | base <= dVal = error "Invalid digit!"
+    | otherwise = mapDecToHex dVal
+
+mapDecToHex :: Int -> Char
+mapDecToHex x
+    | x < 10 = intToDigit x
+    | x == 10 = 'A'
+    | x == 11 = 'B'
+    | x == 12 = 'C'
+    | x == 13 = 'D'
+    | x == 14 = 'E'
+    | x == 15 = 'F'
+
+-- intToDigit x = show x
+
+-- charsToDigits [mode, base, vals] = putStrLn (mode ++ " " ++ base ++ " " ++ vals)
 
 main = do
     args <- getArgs
     checkArgs args
+
+-- main = do
+--     putStrLn [digitsToChars 16 14]
