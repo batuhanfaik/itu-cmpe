6d3904e437b1785dfeb8c172e320514c67bdfcf5
batuhanfaik <batuhanfaik@gmail.com>
2021-05-14 12:53:14 +0300

    Finish similarity matrix

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -11,8 +11,16 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+
+#define MATCH 1
+#define MISMATCH -1
+#define GAP -2
 
 using namespace std;
+
+int score(const char&, const char&);
+vector<vector<int>> similarity_matrix(const string&, const string&);
 
 int main() {
   ifstream file;
@@ -23,11 +31,52 @@ int main() {
     exit(1);
   }
 
+  vector<string> lines;
   string line;
   // Read first N lines
-  while (!file.eof()) {
-    // Read strings from file
-    getline(file, line, '\n'); //line (string)
-    cout << line << endl;
+//  while (!file.eof()) {
+//    // Read strings from file
+//    getline(file, line, '\n'); //line (string)
+//    lines.push_back(line); // Add line to the lines
+//  }
+
+  vector<vector<int>> sm = similarity_matrix("information", "funimatio");
+  for (const auto& i: sm) {
+    for (const auto& j: i) {
+      cout << j << "\t";
+    }
+    cout << endl;
   }
+
+  // Print lines
+//  for (const auto& e: lines) {
+//    cout << e << endl;
+//  }
+
+  file.close();
+}
+
+int score(const char& c1, const char& c2){
+  if (c1 != c2)
+    return MISMATCH;
+  return MATCH;
+}
+
+vector<vector<int>> similarity_matrix(const string& w1, const string& w2) {
+  string word1 = "-" + w1;
+  string word2 = "-" + w2;
+  vector<vector<int>> matrix(word1.length(), vector<int> (word2.length(), 0));
+
+  // Fill the matrix
+  for (uint i=1; i < word1.length(); i++) {
+    for (uint j=1; j < word2.length(); j++) {
+      matrix[i][j] = max({
+          matrix[i-1][j-1] + score(word1[i], word2[j]),
+          matrix[i-1][j] + GAP,
+          matrix[i][j-1] + GAP,
+          0});
+    }
+  }
+
+  return matrix;
 }
