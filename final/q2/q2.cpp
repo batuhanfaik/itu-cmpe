722083b88e574057d04257ebf3218376f9c70844
batuhanfaik <batuhanfaik@gmail.com>
2021-06-20 10:06:59 +0300

    Fix ulong to int casting

--- final/q2/q2.cpp
+++ final/q2/q2.cpp
@@ -76,8 +76,9 @@ class PointSpace {
   static pair<Point, Point> pairwiseDistance(const vector<Point> &p) {
     double min = distance(p[0], p[1]);
     pair<Point, Point> minPair = make_pair(p[0], p[1]);
-    for (int i = 0; i < p.size(); i++) {
-      for (int j = i+1; j < p.size(); j++) {
+    int pSize = int(p.size());
+    for (int i = 0; i < pSize; i++) {
+      for (int j = i+1; j < pSize; j++) {
         if (distance(p[i], p[j]) < min)
           minPair = make_pair(p[i], p[j]);
       }
