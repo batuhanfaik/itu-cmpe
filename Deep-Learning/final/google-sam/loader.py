8602d43b8afd399c950a71f2f3be17b8da828c47
batuhanfaik <batuhanfaik@gmail.com>
2021-07-10 12:43:03 +0300

    Add DL Final google-sam

---
+++ Deep-Learning/final/google-sam/loader.py
@@ -0,0 +1,254 @@
+import torch
+import glob
+import os
+import torch.utils.data
+from PIL import Image
+from torchvision import transforms
+import numpy as np
+import random
+import cv2
+from skimage import exposure
+import pandas as pd
+
+np.random.seed(1773)
+
+
+def load_input_img(filepath, crx_norm):
+    # Apply CRX normalization
+    if crx_norm:
+        # Read grayscale
+        img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        # Adaptive Histogram Equalization
+        clahe = cv2.createCLAHE(clipLimit=crx_norm["clip_limit"], tileGridSize=crx_norm["tile_grid_size"])
+        img = clahe.apply(img)
+        # Median Filtering
+        img = cv2.medianBlur(img, crx_norm["median_filter_size"])
+        # Contrast Stretching
+        lower_percentile = np.percentile(img, crx_norm["percentiles"][0])
+        upper_percentile = np.percentile(img, crx_norm["percentiles"][1])
+        img = exposure.rescale_intensity(img, in_range=(lower_percentile, upper_percentile))
+        # Back to PIL
+        img = Image.fromarray(img)
+    else:
+        img = Image.open(filepath).convert('L')
+    return img
+
+
+class DataReader(torch.utils.data.Dataset):
+    def __init__(self, mode, path, oversample, multi_class, dataset_path=None, mean=123, std=57, crx_norm=None):
+        super(DataReader, self).__init__()
+
+        self.input_img_paths = []
+        self.input_label = []
+        self.mode = mode
+        self.multi_class = multi_class
+        self.dataset_path = dataset_path
+        self.crx_norm = crx_norm
+
+        df = pd.read_csv(path)
+        df = df.fillna(0)
+        df_to_dict = df.set_index('X_ray_image_name').to_dict()
+
+        self.label = df_to_dict['Label']
+        self.dataset_type = df_to_dict['Dataset_type']
+        self.label_1 = df_to_dict['Label_1_Virus_category']
+        self.label_2 = df_to_dict['Label_2_Virus_category']
+
+        self.mean = mean / 255
+        self.std = std / 255
+
+        self.healthy = []
+
+        if mode == 'train':
+            self.input_transform = transforms.Compose([
+                transforms.Resize(size=224, interpolation=Image.BILINEAR),
+                transforms.CenterCrop(size=(224, 224)),
+                transforms.RandomPerspective(distortion_scale=0.1, interpolation=Image.BILINEAR),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=self.mean, std=self.std)
+            ])
+        # TODO
+        elif mode == 'val':
+            self.input_transform = transforms.Compose([
+                transforms.Resize(size=224, interpolation=Image.BILINEAR),
+                transforms.CenterCrop(size=(224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=self.mean, std=self.std)
+            ])
+        # TODO
+        elif mode == 'test':
+            self.input_transform = transforms.Compose([
+                transforms.Resize(size=224, interpolation=Image.BILINEAR),
+                transforms.CenterCrop(size=(224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=self.mean, std=self.std)
+            ])
+
+        if mode == 'train':
+
+            # train_input_folder = os.listdir("train")
+            train_input_folder = df['X_ray_image_name']
+            train_input_folder = sorted(train_input_folder)
+
+            normal_counter = 0
+            corona_counter = 0
+
+            for i in range(len(train_input_folder)):
+                if self.dataset_type[train_input_folder[i]] == "TRAIN":
+                    temp_path = "train/" + train_input_folder[i]  # input image's path
+                    if self.dataset_path:
+                        temp_path = os.path.join(self.dataset_path, temp_path)
+
+                    if multi_class == True and self.label_1[train_input_folder[i]] != "Stress-Smoking":
+                        self.input_img_paths.append(temp_path)
+                    elif multi_class == False:
+                        self.input_img_paths.append(temp_path)
+
+                    if multi_class == True:
+                        if self.label_1[train_input_folder[i]] == "Virus":
+                            self.input_label.append(1)
+                            corona_counter += 1
+                        elif self.label_1[train_input_folder[i]] == "bacteria":
+                            self.input_label.append(2)
+                            corona_counter += 1
+                        elif self.label_1[train_input_folder[i]] == "Stress-Smoking":
+                            #self.input_label.append(3)
+                            #corona_counter += 1
+                            pass
+                        else:
+                            self.input_label.append(0)
+                            self.healthy.append(temp_path)
+                            normal_counter += 1
+
+                    else:
+                        if self.label[train_input_folder[i]] == "Normal":
+                            self.input_label.append(0)
+                            self.healthy.append(temp_path)
+                            normal_counter += 1
+                        else:
+                            self.input_label.append(1)
+                            corona_counter += 1
+
+            if oversample == True:
+                num_size = corona_counter - normal_counter
+                random_numbers = np.random.randint(len(self.healthy), size=num_size)
+
+                for i in random_numbers:
+                    self.input_img_paths.append(self.healthy[i])
+                    self.input_label.append(0)
+                    normal_counter += 1
+
+        elif mode == 'val':
+
+            # val_input_folder = os.listdir("train")
+            val_input_folder = df['X_ray_image_name']
+            val_input_folder = sorted(val_input_folder)
+
+            normal_counter = 0
+            corona_counter = 0
+
+            for i in range(len(val_input_folder)):
+                if self.dataset_type[val_input_folder[i]] == "VAL":
+                    temp_path = "train/" + val_input_folder[i]  # input image's path
+                    if self.dataset_path:
+                        temp_path = os.path.join(self.dataset_path, temp_path)
+
+                    if multi_class == True and self.label_1[val_input_folder[i]] != "Stress-Smoking":
+                        self.input_img_paths.append(temp_path)
+                    elif multi_class == False:
+                        self.input_img_paths.append(temp_path)
+
+                    if multi_class == True:
+                        if self.label_1[val_input_folder[i]] == "Virus":
+                            self.input_label.append(1)
+                            corona_counter += 1
+                        elif self.label_1[val_input_folder[i]] == "bacteria":
+                            self.input_label.append(2)
+                            corona_counter += 1
+                        elif self.label_1[val_input_folder[i]] == "Stress-Smoking":
+                            #self.input_label.append(3)
+                            #corona_counter += 1
+                            pass
+                        else:
+                            self.input_label.append(0)
+                            self.healthy.append(temp_path)
+                            normal_counter += 1
+
+                    else:
+                        if self.label[val_input_folder[i]] == "Normal":
+                            self.input_label.append(0)
+                        else:
+                            self.input_label.append(1)
+
+        elif mode == 'test':
+            # test_input_folder = os.listdir("test")
+            test_input_folder = df['X_ray_image_name']
+            test_input_folder = sorted(test_input_folder)
+
+            normal_counter = 0
+            corona_counter = 0
+
+            for i in range(len(test_input_folder)):
+                if self.dataset_type[test_input_folder[i]] == "TEST":
+                    temp_path = "test/" + test_input_folder[i]  # input image's path
+                    if self.dataset_path:
+                        temp_path = os.path.join(self.dataset_path, temp_path)
+                    if multi_class == True and self.label_1[test_input_folder[i]] != "Stress-Smoking":
+                        self.input_img_paths.append(temp_path)
+                    elif multi_class == False:
+                        self.input_img_paths.append(temp_path)
+
+                    if multi_class == True:
+                        if self.label_1[test_input_folder[i]] == "Virus":
+                            self.input_label.append(1)
+                            corona_counter += 1
+                        elif self.label_1[test_input_folder[i]] == "bacteria":
+                            self.input_label.append(2)
+                            corona_counter += 1
+                        elif self.label_1[test_input_folder[i]] == "Stress-Smoking":
+                            #self.input_label.append(3)
+                            #corona_counter += 1
+                            pass
+                        else:
+                            self.input_label.append(0)
+                            normal_counter += 1
+
+                    else:
+                        if self.label[test_input_folder[i]] == "Normal":
+                            self.input_label.append(0)
+                        else:
+                            self.input_label.append(1)
+
+    def __getitem__(self, index):
+        if self.mode == 'train':
+            img = load_input_img(self.input_img_paths[index], self.crx_norm)
+            label = self.input_label[index]
+
+            img = self.input_transform(img)
+
+            data = {'image': img, 'label': label}
+
+            return data
+
+        elif self.mode == 'val':
+            img = load_input_img(self.input_img_paths[index], self.crx_norm)
+            label = self.input_label[index]
+
+            img = self.input_transform(img)
+
+            data = {'image': img, 'label': label}
+
+            return data
+
+        elif self.mode == 'test':
+            img = load_input_img(self.input_img_paths[index], self.crx_norm)
+            label = self.input_label[index]
+
+            img = self.input_transform(img)
+
+            data = {'image': img, 'label': label}
+
+            return data
+
+    def __len__(self):
+        return len(self.input_img_paths)
---
+++ Deep-Learning/final/google-sam/train.py
@@ -0,0 +1,391 @@
+import shutil
+import numpy as np
+import torch
+import time
+import re
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from loader import DataReader
+import torch.nn as nn
+import os
+import datetime
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from torchvision import datasets, models, transforms
+from collections import OrderedDict
+from sklearn.metrics import confusion_matrix, classification_report
+from densenet import densenet121
+import resnet
+from preprocessor import Preprocessor
+from sam import SAM
+
+
+def conf_matrix(cmat):
+    arr = cmat.copy()
+    tp = 0.0
+    tn = 0.0
+    fp = 0.0
+    fn = 0.0
+    for i in range(arr.shape[0]):
+        tp += arr[i, i]
+        tn += arr.sum() - arr[:, i].sum() - arr[i, :].sum() + arr[i, i]
+        fn += arr[i, :].sum() - arr[i, i]
+        fp += arr[:, i].sum() - arr[i, i]
+
+    return tn, fp, fn, tp
+
+
+def save_res(epoch_id, total_loss, loader_len, report, time_start, res_name, mode, multi_class, multi_to_multi):
+    with open(res_name, "a") as f:
+        f.write(mode)
+        f.write(": ")
+        f.write(str(datetime.datetime.now()))
+        f.write("\n")
+
+        f.write("Epoch ")
+        f.write(" scores: ")
+        f.write(str(epoch_id))
+        f.write("\n")
+
+        f.write("Loss: ")
+        f.write(str((total_loss / loader_len)))
+        f.write("\n")
+        f.write("\n")
+        f.write(report)
+        f.write("\n")
+        f.write("Time (s): ")
+        f.write(str(time.time() - time_start))
+        f.write("\n")
+        f.write("--------------------------------------")
+        f.write("\n")
+
+
+def prepare_experiment(project_path=".", experiment_name=None):
+    next_experiment_number = 0
+    for directory in os.listdir(project_path):
+        search_result = re.search("experiment_(.*)", directory)
+        if search_result and next_experiment_number < int(search_result[1]) + 1:
+            next_experiment_number = int(search_result[1]) + 1
+
+    if not experiment_name:
+        exp_name = "experiment_{}".format(next_experiment_number)
+    else:
+        exp_name = experiment_name
+        if os.path.exists(experiment_name) and os.path.isdir(experiment_name):
+            shutil.rmtree(exp_name)
+
+    os.mkdir(exp_name)
+    os.mkdir(exp_name + '/graphs/')
+    os.mkdir(exp_name + '/models/')
+    os.mkdir(exp_name + '/code/')
+
+    return exp_name
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+BATCH_SIZE = 64
+num_workers = 1
+
+'''
+multi class classification:
+    multi_to_multi = True
+    multi_class = True
+multi to binary classification:
+    multi_to_multi = False
+    multi_class = True
+binary classification:
+    multi_to_multi = False
+    multi_class = False
+'''
+dataset_path = "/mnt/sdb1/datasets/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset"
+preprocessed_dataset_path = "/mnt/sdb1/datasets/Coronahack-Chest-XRay-Dataset/preprocessed_dataset"
+split_path = "splits/split_0.8-0.2.csv"
+#####################################################
+# CRX Normalization parameters
+#####################################################
+crx_norm = {
+    "clip_limit": 2.0,
+    "tile_grid_size": (8, 8),
+    "median_filter_size": 5,
+    "percentiles": (2, 98)
+}
+#####################################################
+# Dataset Preprocessing
+#####################################################
+# preprocessor = Preprocessor(dataset_path=dataset_path, crx_params=crx_norm, mode="c")
+# dataset_path, dataset_mean, dataset_std = preprocessor.preprocess_dataset()
+#####################################################
+# Default mean and std values
+#####################################################
+# dataset_mean = 123
+# dataset_std = 57
+#####################################################
+# Preprocessed mean and std values
+#####################################################
+dataset_mean = 143
+dataset_std = 72
+dataset_path = preprocessed_dataset_path
+#####################################################
+multi_to_multi = True
+multi_class = True
+
+oversample = True
+#####################################################
+
+train_loader = torch.utils.data.DataLoader(
+    DataReader(mode='train', path=split_path, dataset_path=dataset_path, oversample=oversample,
+               multi_class=multi_class, mean=dataset_mean, std=dataset_std, crx_norm=None),
+    batch_size=BATCH_SIZE, shuffle=True, num_workers=num_workers)
+
+val_loader = torch.utils.data.DataLoader(
+    DataReader(mode='val', path=split_path, dataset_path=dataset_path, oversample=oversample,
+               multi_class=multi_class, mean=dataset_mean, std=dataset_std, crx_norm=None),
+    batch_size=BATCH_SIZE, shuffle=False, num_workers=num_workers)
+
+experiment_name = prepare_experiment(experiment_name="densenet_multi_multi_crx_oversample_sam")
+res_name = experiment_name + "/" + experiment_name + "_res.txt"
+
+all_python_files = os.listdir('.')
+
+for i in range(len(all_python_files)):
+    if '.py' in all_python_files[i]:
+        os.system('cp ' + all_python_files[i] + ' ' + experiment_name + '/code/')
+
+num_classes = 5
+num_epochs = 25
+
+# model = resnet.resnet101(pretrained=False)
+# num_features_resnet = model.in_features
+#
+# if multi_class == True:
+#     model.fc = nn.Linear(num_features_resnet, 3)
+# else:
+#     model.fc = nn.Linear(num_features_resnet, 1)
+
+model = densenet121(pretrained=False)
+num_features_dense = model.classifier.in_features
+
+if multi_class == True:
+    model.classifier = nn.Linear(num_features_dense, 3)
+else:
+    model.classifier = nn.Linear(num_features_dense, 1)
+
+model = model.to(device)
+
+lr = 1e-3
+base_optimizer = torch.optim.SGD
+# base_optimizer = torch.optim.Adam
+# optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4, nesterov=True)
+optimizer = SAM(model.parameters(), base_optimizer, lr=0.1, momentum=0.9, weight_decay=1e-4, nesterov=True)
+scheduler = ReduceLROnPlateau(optimizer, 'min', factor=0.1, patience=5, min_lr=1e-10)
+
+if multi_class == True:
+    loss = torch.nn.CrossEntropyLoss().to(device)
+    loss_test = torch.nn.BCEWithLogitsLoss().to(device)
+else:
+    loss = torch.nn.BCEWithLogitsLoss().to(device)
+
+
+all_tr_losses = torch.zeros(num_epochs, 1)
+all_test_losses = torch.zeros(num_epochs, 1)
+
+
+for epoch_id in range(1, num_epochs + 1):
+    model.train()
+
+    total_loss = 0
+    y_true = None
+    y_pred = None
+    time_start = time.time()
+
+    for i, data in enumerate(train_loader):
+        img = data['image']
+        img_class = data['label']
+        img = img.float()
+        if multi_class == False:
+            img_class = img_class.float()
+
+        img = img.to(device)
+        img.requires_grad = True
+
+        img_class = img_class.to(device)
+        img_class.requires_grad = False
+
+        output = model(img)
+        if multi_class == True:
+            loss_value = loss(output, img_class)
+            prediction = torch.argmax(output.data, 1)
+            
+        else:
+            loss_value = loss(output, img_class.unsqueeze(1))    # if multiclass false
+            res = torch.sigmoid(output)
+            img_class = img_class.int()
+            temp_output = res
+            temp_output = (temp_output > 0.5).int()
+            prediction = temp_output
+
+        if y_pred is None:
+            y_pred = prediction.cpu().numpy().flatten()
+        else:
+            y_pred = np.concatenate([y_pred, prediction.cpu().numpy().flatten()])
+        
+        if y_true is None:
+            y_true = img_class.cpu().numpy().flatten()
+        else:
+            y_true = np.concatenate([y_true, img_class.cpu().numpy().flatten()])   
+
+        loss_value.backward()
+        optimizer.first_step(zero_grad=True)
+        # Second pass
+        if multi_class == True:
+            loss_value = loss(model(img), img_class)
+        else:
+            img_class = img_class.float()
+            loss_value = loss(model(img), img_class.unsqueeze(1))
+            img_class = img_class.int()
+        loss_value.backward()
+        optimizer.second_step(zero_grad=True)
+
+        total_loss += loss_value.data
+        
+        if (i + 1) % 100 == 0:
+            print("Pre-report Epoch:", epoch_id)
+            print("Loss: %f" % loss_value.data)
+            print("Status -> %d / %d" % (i + 1, len(train_loader)))
+            print("************************************")
+    
+    if multi_class == True:
+        report = classification_report(y_true, y_pred, labels = [0,1,2], output_dict=True)
+        report_to_print = classification_report(y_true, y_pred, labels = [0,1,2], output_dict=False)
+    else:
+        report = classification_report(y_true, y_pred, labels = [0,1], output_dict=False)
+        report_to_print = classification_report(y_true, y_pred, labels = [0,1], output_dict=False)
+    
+    print("Train:", datetime.datetime.now())
+    print("Epoch %d scores:" % epoch_id)
+    print("Loss: %f" % (total_loss / len(train_loader))) 
+    print("Report: \n", report_to_print)
+    print("LR: %f" % get_lr(optimizer))
+    print("Time (s): " + str(time.time() - time_start))
+    print("--------------------------------------")
+
+    # all_tr_losses[epoch_id] = total_loss.cpu()
+    all_tr_losses[epoch_id-1] = total_loss / len(train_loader)
+
+
+    save_res(epoch_id, total_loss, len(train_loader), report_to_print, time_start, res_name, "train", multi_class, multi_to_multi)
+    
+    with torch.no_grad():
+
+        model.eval()
+
+        val_losses = 0
+        total_loss = 0
+        y_true = None
+        y_pred = None
+        time_start = time.time()
+
+        for i, batch in enumerate(val_loader):
+            img = batch['image']
+            img_class = batch['label']
+            img = img.float()
+            img = img.to(device)
+            if multi_class == False:
+                img_class = img_class.float()
+            img_class = img_class.to(device)
+            output = model(img)
+
+            if multi_to_multi == True:
+                prediction = torch.argmax(output.data, 1)
+                val_loss = loss(output, img_class)
+                
+            elif multi_class == True:  # multi to binary
+                
+
+                temp_list = []
+                temp_list = torch.cat([output[:, 0:1].sum(dim=1, keepdim=True),
+                                       output[:, 1:3].sum(dim=1, keepdim=True)], dim=1)
+                new_output = temp_list
+                val_loss = loss(output, img_class)
+                res = torch.sigmoid(new_output)
+
+                #img_class = img_class.detach().cpu().numpy()
+                #img_class = img_class.flatten().astype(np.int)
+                img_class[img_class > 0] = 1
+                
+
+                prediction = torch.argmax(new_output.data, 1)
+                
+                    
+            else:  # binary
+                val_loss = loss(output, img_class.unsqueeze(1))
+                res = torch.sigmoid(output)
+                img_class = img_class.int()
+                temp_output = res
+                temp_output = (temp_output > 0.5).int()
+                prediction = temp_output
+
+            if y_pred is None:
+                y_pred = prediction.cpu().numpy().flatten()
+            else:
+                y_pred = np.concatenate([y_pred, prediction.cpu().numpy().flatten()])
+            
+            if y_true is None:
+                y_true = img_class.cpu().numpy().flatten()
+            else:
+                y_true = np.concatenate([y_true, img_class.cpu().numpy().flatten()])                   
+
+
+            val_losses += val_loss
+            total_loss += val_loss.data
+
+        if multi_to_multi == True:
+            report = classification_report(y_true, y_pred, labels = [0,1,2], output_dict=True)
+            report_to_print = classification_report(y_true, y_pred, labels = [0,1,2], output_dict=False)
+        else:
+            report = classification_report(y_true, y_pred, labels = [0,1], output_dict=True)
+            report_to_print = classification_report(y_true, y_pred, labels = [0,1], output_dict=False)
+
+
+        model_path = experiment_name + "/models/model_epoch_" + str(epoch_id) + '.pt'
+        torch.save(model, model_path)            
+
+        print("Test:", datetime.datetime.now())
+        print("Val %d scores:" % epoch_id)
+        print("Loss %f" % (total_loss / len(val_loader)))
+        print("Report:\n ", report_to_print)
+        print("Time (s): " + str(time.time() - time_start))
+        print("--------------------------------------")
+
+        # all_test_losses[epoch_id] = total_loss.cpu()
+        all_test_losses[epoch_id-1] = total_loss / len(val_loader)
+
+    scheduler.step(val_losses / len(val_loader))
+
+    save_res(epoch_id, total_loss, len(train_loader), report_to_print, time_start, res_name, "train", multi_class, multi_to_multi)
+
+    training_loss = all_tr_losses.numpy()
+    training_loss = np.reshape(training_loss, (training_loss.shape[1] * training_loss.shape[0], -1))
+
+    val_loss = all_test_losses.numpy()
+    val_loss = np.reshape(val_loss, (val_loss.shape[1] * val_loss.shape[0], -1))
+
+    training_loss[training_loss == 0] = np.nan
+    val_loss[val_loss == 0] = np.nan
+
+    plt.plot(training_loss, label='Train')
+    plt.plot(val_loss, label='Validation')
+    plt.title("Train - Validation Loss Curve")
+    plt.ylabel('Loss')
+    plt.xlabel('# Epochs')
+    plt.legend()
+    plt.grid(True)
+    fig_path = experiment_name + "/graphs/train_val_loss_epoch_" + str(epoch_id) + '.png'
+    plt.savefig(fig_path)
+    plt.clf()
